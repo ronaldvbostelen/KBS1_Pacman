6866e304fd0d417d1e937bee58fce5da2c9c2b0a
Karen Bosch - Brakband <info@karenbosch.net>
2018-11-17 17:00:43 +0100

    Adds a basic Timer/Clock and a Score class. This may need to be done differently in the future, but it's a start.

---
+++ WpfGame/WpfGame/Controllers/Clock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WpfGame.Controllers
+{
+    class Clock
+    {
+        private Timer timer;
+
+        public void Initialize()
+        {
+            timer = new Timer();
+            timer.Elapsed += Timer_Elapsed;
+            timer.Interval = 60000;
+            timer.Enabled = true;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Timer is gestopt");
+        }
+    }
+}
