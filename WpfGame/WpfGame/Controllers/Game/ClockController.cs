89cc32dcb02d6df3e3bac047f86d79d8c31b4db1
Karen Bosch - Brakband <info@karenbosch.net>
2018-11-24 20:49:44 +0100

    60 seconds instead of 1 second

--- WpfGame/WpfGame/Controllers/Game/ClockController.cs
+++ WpfGame/WpfGame/Controllers/Game/ClockController.cs
@@ -37,7 +37,7 @@ namespace WpfGame.Controllers
         {
             _timer = new DispatcherTimer();
 
-            _time = TimeSpan.FromSeconds(1); // Count down from 60 seconds
+            _time = TimeSpan.FromSeconds(60); // Count down from 60 seconds
 
             // Call this every 1 second
             _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
