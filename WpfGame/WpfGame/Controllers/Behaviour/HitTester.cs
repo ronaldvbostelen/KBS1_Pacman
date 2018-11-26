50cdf91439eb27bf1efea37b0a97f04e084d2d13
DESKTOP-P03HJ5A\rvanb <rvanbostelen@outlook.com>
2018-11-26 18:59:10 +0100

    naming

--- WpfGame/WpfGame/Controllers/Behaviour/HitTester.cs
+++ WpfGame/WpfGame/Controllers/Behaviour/HitTester.cs
@@ -17,29 +17,29 @@ namespace WpfGame.Controllers.Behaviour
             _gameValues = gameValues;
         }
 
-        public bool HitLeftBorderOfPlayground(double xOfObject, double nextMove) => xOfObject - nextMove <= 0;
+        public bool LeftBorderOfPlaygroundCollision(double xOfObject, double nextMove) => xOfObject - nextMove <= 0;
 
-        public bool HitRightBorderOfPlayground(double playgroundWidth, double objectWidth, double xOfObject,
+        public bool RightBorderOfPlaygroundCollision(double playgroundWidth, double objectWidth, double xOfObject,
             double nextMove) => xOfObject + nextMove + objectWidth >= playgroundWidth;
 
-        public bool HitTopBorderOfPlayground(double yOfObject, double nextMove) => yOfObject - nextMove <= 0;
+        public bool TopBorderOfPlaygroundCollision(double yOfObject, double nextMove) => yOfObject - nextMove <= 0;
 
-        public bool HitBottomBorderOfPlayground(double playgroundHeight, double objectHeight,
+        public bool BottomBorderOfPlaygroundCollision(double playgroundHeight, double objectHeight,
             double yOfObject, double nextMove) => yOfObject + objectHeight + nextMove >= playgroundHeight;
 
         public bool BorderHit(Sprite sprite, Move move)
-        {
+        {   
             switch (move)
             {
                 case Move.Up:
-                    return HitTopBorderOfPlayground(sprite.Y, _gameValues.UpDownMovement);
+                    return TopBorderOfPlaygroundCollision(sprite.Y, _gameValues.UpDownMovement);
                 case Move.Down:
-                    return HitBottomBorderOfPlayground(_gameValues.PlayCanvasHeight, sprite.Image.Height, sprite.Y,
+                    return BottomBorderOfPlaygroundCollision(_gameValues.PlayCanvasHeight, sprite.Image.Height, sprite.Y,
                         _gameValues.UpDownMovement);
                 case Move.Left:
-                    return HitLeftBorderOfPlayground(sprite.X, _gameValues.LeftRightMovement);
+                    return LeftBorderOfPlaygroundCollision(sprite.X, _gameValues.LeftRightMovement);
                 case Move.Right:
-                    return HitRightBorderOfPlayground(_gameValues.PlayCanvasWidth, sprite.Image.Width, sprite.X,
+                    return RightBorderOfPlaygroundCollision(_gameValues.PlayCanvasWidth, sprite.Image.Width, sprite.X,
                         _gameValues.LeftRightMovement);
                 default:
                     return false;
